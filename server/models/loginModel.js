8f57626c2391e951c9b046285ce94f69ccb3dc8b
Jubalme <bethelasnake0000@gmail.com>
2024-07-31 15:32:19 +0300

    saving userrs to database

---
+++ server/models/loginModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const loginSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      minlength: 3,
+    },
+
+    password: {
+      type: String,
+      specialchars: true,
+      required: true,
+      minlength: 5,
+    },
+    email: {
+      type: String,
+      specialchars: true,
+    },
+    first_name: {
+      type: String,
+    },
+    last_name: {
+      type: String,
+    },
+    date_of_birth: {
+      type: Number,
+      specialchars: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const user = mongoose.model("user", loginSchema);
+module.exports=user;
--- server/server.js
+++ server/server.js
@@ -3,20 +3,29 @@ const app= express();
 
 require('dotenv').config();
 
-//const connectDB=require('./utilities/dbconnection');
-//connectDB();
+const connectDB=require('./utilities/dbconnection');
+connectDB();
 const port = process.env.PORT || 4040;
 app.use(express.json());
-
+const user= require('./models/loginModel.js');
 
 const final= require('./routes/user_register.js');
-const hey=require('./routes/emailroute');
+//const hey=require('./routes/emailroute');
 
-const email = require('./routes/emailroute.js');
+//const email = require('./routes/emailroute.js');
 app.use('/admin', final);
-app.use('/test',hey )
+//app.use('/test',hey )
+app.post('/api/viewModel',async (req,res)=>{
+    try{
+      const saved=  await user.create(req.body);
+      res.status(200).json(saved);
+    }
+    catch(error){
+        res.status(500).json({message: error.message});
+    }
+});
 
-app.use( '/api', email);
+//app.use( '/api', email);
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
