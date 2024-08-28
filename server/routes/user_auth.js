d97b404c49bb92c35761375e40a9ce29f3df4e57
Jubalme <bethelasnake0000@gmail.com>
2024-08-28 19:40:04 +0300

    ..

--- server/routes/user_auth.js
+++ server/routes/user_auth.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router= express.Router();
-
 const signup=require('../controllers/register.js' )
-const emails=require('./emailroute.js')
+
 router.post('/signup',signup);
-// router.get('/emailsend',emails);
+
 
 
 
--- server/server.js
+++ server/server.js
@@ -5,18 +5,15 @@ require('dotenv').config();
 
 const connectDB = require('./utilities/dbconnection');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
 connectDB();
 
-
-const user= require('./models/loginModel.js');
-
 const User= require('./routes/user_auth.js');
 
-app.use('/api/user', User);
+app.use('/api/user',User);
 
 app.listen(4545, () => {
     console.log(`Server running on port ${port}`)
